01005f2ef77ea53ece19fa5e95c256ba232a5862
Naradhrishi <kumarnarad2111@gmail.com>
2026-05-30 02:43:35 +0530

    Added Multiplication feature in my Calculator app and it passed all tests using pytest

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
 def add(a, b):
   return a+b
 
+def mul(x, y):
+  return x*y
--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,21 @@
 from calculator import add
+from calculator import mul
 
 def test_add_fun_pos():
   assert add(2,3) == 5
 
 def test_add_fun_neg():
   assert add(-1,-2) == -3
+  
+def test_mul_fun_pos():
+  assert mul(2,7) == 14
+  
+def test_mul_fun_neg():
+  assert mul(-3,-7) == 21
+  
+def test_mul_fun_mix():
+  assert mul(-5,9) == -45
+
+
   
 
